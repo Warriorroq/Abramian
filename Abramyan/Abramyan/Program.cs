16534230f4a8a486bd037b2e4b5df2082b93622d
Warriorroq <Warriorroq@gmail.com>
2021-04-03 11:51:28 +0300

    SetUp

---
+++ Abramyan/Abramyan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+namespace Abramyan
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        private static void For11(int N)
+        {
+            
+        }
+    }
+}
