8f766ce95db895398b7773a3963583b99ba0e968
Warriorroq <Warriorroq@gmail.com>
2021-04-03 12:52:56 +0300

    task

--- Abramyan/Abramyan/Program.cs
+++ Abramyan/Abramyan/Program.cs
@@ -9,7 +9,8 @@ namespace Abramyan
         {
             Console.WriteLine(For11(5));
             Console.WriteLine(Series8(new int[] { 2, 4, 56, 73, 2, 4, 1 }));
-            Console.WriteLine(Array3(4,0,2));
+            Console.WriteLine(Array3(4,1,2));
+            Console.WriteLine(Array11(3, new int[] { 2, 4, 56, 73, 2, 4, 1 , 3, 5, 6, 7, 9, 10}));
         }
         private static int For11(int N)
             => Enumerable.Range(N, 2 * N).Select(x => x * x).Sum();
@@ -20,6 +21,10 @@ namespace Abramyan
         private static string Array3(int countOfNUms, int firstNum, int delta)
             => string.Join(' ', ArihmeticProgress(countOfNUms, firstNum, delta));
         private static IEnumerable<int> ArihmeticProgress(int countOfNums, int firstNum, int delta)
-            => Enumerable.Range(firstNum, firstNum + countOfNums).Select(x => x + delta);
+            => Enumerable.Range(0, countOfNums).Select(x => firstNum + delta * x);
+        private static string Array11(int K, params int[] arr)
+            => string.Join(' ',TakeKThings(K, arr));
+        private static IEnumerable<int> TakeKThings(int K, params int[] arr)
+            => Enumerable.Range(0, arr.Length / K).Select(x => arr[x * K]);
     }
 }
